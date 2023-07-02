dda3bb4883fa8c043a8328de0ca0857257840e99
Igor Olkhov <smartestbox@gmail.com>
2023-07-03 00:02:36 +0200

    Create board with squares

--- src/App.tsx
+++ src/App.tsx
@@ -3,15 +3,33 @@ import logo from './logo.svg';
 import './App.css';
 
 function App() {
-  return (
-    <div className="App">
-
-    </div>
-  );
+    return (
+        <div className="App">
+            <Board/>
+        </div>
+    );
 }
 
-export function Square() {
-  return <button className="square">X</button>;
+export function Board() {
+    return <>
+        <div className='board-row'>
+            <button className="square">1</button>
+            <button className="square">2</button>
+            <button className="square">3</button>
+        </div>
+        <div className='board-row'>
+            <button className="square">4</button>
+            <button className="square">5</button>
+            <button className="square">6</button>
+        </div>
+        <div className='board-row'>
+            <button className="square">7</button>
+            <button className="square">8</button>
+            <button className="square">9</button>
+        </div>
+    </>
 }
+
+
 
 export default App;
