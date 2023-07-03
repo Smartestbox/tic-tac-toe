4c30c55432dfb4fd7fb32e8bdf93fe56558d22fd
Igor Olkhov <smartestbox@gmail.com>
2023-07-03 02:33:36 +0200

    Add calculator to check winner

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,11 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import {useState} from 'react'
+
+export type SquarePropsType = {
+    value: string | null
+    onSquareClick: () => void
+}
 
 function App() {
     return (
@@ -11,25 +16,73 @@ function App() {
 }
 
 export function Board() {
-    return <>
-        <div className='board-row'>
-            <button className="square">1</button>
-            <button className="square">2</button>
-            <button className="square">3</button>
-        </div>
-        <div className='board-row'>
-            <button className="square">4</button>
-            <button className="square">5</button>
-            <button className="square">6</button>
-        </div>
-        <div className='board-row'>
-            <button className="square">7</button>
-            <button className="square">8</button>
-            <button className="square">9</button>
-        </div>
-    </>
+    const [squares, setSquares] = useState<Array<string | null>>(Array(9).fill(null))
+    const [xIsNext, setXIsNext] = useState<boolean>(true)
+    const handleClick = (i: number) => {
+        if(squares[i]) {
+            return
+        }
+
+        const nextSquares = squares.slice()
+
+        if(xIsNext) {
+            nextSquares[i] = 'X'
+        } else {
+            nextSquares[i] = 'O'
+        }
+
+        setSquares(nextSquares)
+        setXIsNext(!xIsNext)
+    }
+    return (
+        <>
+            <div className='board-row'>
+                <Square value={squares[0]} onSquareClick={()=>handleClick(0)}/>
+                <Square value={squares[1]} onSquareClick={()=>handleClick(1)}/>
+                <Square value={squares[2]} onSquareClick={()=>handleClick(2)}/>
+            </div>
+            <div className='board-row'>
+                <Square value={squares[3]} onSquareClick={()=>handleClick(3)}/>
+                <Square value={squares[4]} onSquareClick={()=>handleClick(4)}/>
+                <Square value={squares[5]} onSquareClick={()=>handleClick(5)}/>
+            </div>
+            <div className='board-row'>
+                <Square value={squares[6]} onSquareClick={()=>handleClick(6)}/>
+                <Square value={squares[7]} onSquareClick={()=>handleClick(7)}/>
+                <Square value={squares[8]} onSquareClick={()=>handleClick(8)}/>
+            </div>
+        </>
+    )
 }
 
+export const Square: React.FC<SquarePropsType> = ({
+                                                      value,
+                                                      onSquareClick
+}) => {
 
+    return (
+        <button className="square" onClick={onSquareClick}>{value}</button>
+    )
+}
+
+function calculateWinner(squares: Array<string | null>) {
+    const lines = [
+        [0, 1, 2],
+        [3, 4, 5],
+        [6, 7, 8],
+        [0, 3, 6],
+        [1, 4, 7],
+        [2, 5, 8],
+        [0, 4, 8],
+        [2, 4, 6]
+    ];
+    for (let i = 0; i < lines.length; i++) {
+        const [a, b, c] = lines[i];
+        if (squares[a] && squares[a] === squares[b] && squares[a] === squares[c]) {
+            return squares[a];
+        }
+    }
+    return null;
+}
 
 export default App;
